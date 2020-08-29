94b4d1ef36e486ae3b7cdae3e57da77f502563c5
Javier Chávez <hrchavez@est.utn.ac.cr>
2020-08-28 22:54:20 -0600

    add photo gallery

---
+++ src/app/services/photo.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PhotoService } from './photo.service';
+
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
+  CameraPhoto, CameraSource } from '@capacitor/core';
+
+const { Camera, Filesystem, Storage } = Plugins;
+
+describe('PhotoService', () => {
+  let service: PhotoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PhotoService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
